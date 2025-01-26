932b2b6afbb7a5948f5d60c07d30413452ef7c87
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-27 04:07:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,90 @@
 //your JS code here. If required.
+btn.addEventListener("click", () => {
+	
+});
+
+// let num = parseInt(prompt());
+let inputNum = document.getElementById("ip");
+let outputNum = document.getElementById("output");
+
+let promise = new Promise((resolve, reject) =>{
+	setTimeout(() => {
+		inputNum.value = num;
+		
+		resolve(num);
+	}, 2000);
+});
+
+let promise1 = (data) =>{ 
+	return new Promise((resolve, reject) =>{
+		setTimeout(() => {
+			inputNum.value = num;
+			// outputNum.textContent = `Result: ${outputNum}`;
+			resolve(num);
+		}, 2000);
+	});
+}
+
+let promise2 = (data) => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			data *= 2;
+			inputNum.value = data;
+			resolve(data);
+		}, 1000);
+	})
+}
+
+let promise4 = (data) => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			data -= 3;
+			inputNum.value = data;
+			resolve(data);
+		}, 1000);
+	})
+}
+
+let promise4 = (data) => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			data /= 2;
+			inputNum.value = data;
+			resolve(data);
+		}, 1000);
+	})
+}
+
+let promise5 = (data) => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			data += 10;
+			inputNum.value = data;
+			resolve(data);
+		}, 1000);
+	})
+}
+
+promise.then(data => {
+	outputNum.textContent = `Result: ${data}`;
+	return promise1;
+})
+.then(data => {
+	outputNum.textContent = `Result: ${data}`;
+	return promise2;
+})
+.then(data => {
+	outputNum.textContent = `Result: ${data}`;
+	return promise3;
+})
+.then(data => {
+	outputNum.textContent = `Result: ${data}`;
+	return promise4;
+})
+.then(data => {
+	outputNum.textContent = `Result: ${data}`;
+	return promise5;
+})
+.then(data => {
+	outputNum.textContent = `Final Result: ${data}`;
+})
